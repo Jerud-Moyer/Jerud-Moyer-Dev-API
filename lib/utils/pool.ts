c68ba09c50f5b49f543e7bae4124eb330fb6dcfa
Jerud-Moyer <jerudmoyer@gmail.com>
2025-01-26 18:22:41 -0800

    and again

--- lib/utils/pool.ts
+++ lib/utils/pool.ts
@@ -2,7 +2,7 @@ import { Pool, Client } from 'pg'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.PGSSLMODE && { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false }
 })
 
 pool.on('connect', () => console.log('POSTGRES Connected!'))
