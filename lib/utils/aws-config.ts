4fca1d93c863afe0eecb4c89aa2d44537a831565
Jerud-Moyer <jerudmoyer@gmail.com>
2025-02-16 16:16:55 -0800

    fix cloudinary service, remove Node-Fetch and clean things up

--- lib/utils/aws-config.ts
+++ lib/utils/aws-config.ts
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk'
 
 export default new AWS.S3({
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
