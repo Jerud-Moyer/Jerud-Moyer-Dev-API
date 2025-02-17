c72f94094ba16e0ffc1d9b1c1a69c8bb52437835
Jerud-Moyer <jerudmoyer@gmail.com>
2025-02-16 19:51:10 -0800

    add trekdex

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,7 @@
 import express, {  Express, Request, Response } from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors';
+import path from 'path'
 import mailController from './mailer/controllers/email'
 import authController from './auth/controllers/auth'
 import factsController from './portfolio/controllers/fact'
@@ -8,6 +9,7 @@ import announcementsController from './amphead/controllers/announcements'
 import artworksController from './eskart/controllers/artwork'
 import seriesController from './eskart/controllers/series'
 import memeController from './nerdmeme/controllers/meme'
+import trekDexController from './trekdex/controllers/main'
 
 export const app: Express = express()
 
@@ -17,9 +19,13 @@ app.use(cors({
   origin: true,
   credentials: true
 }))
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('WE GOT AN EXPRESS TS APP!')
+  res.send('WELCOME TO THE JM-DEV MEGA-API!')
 })
 
 // mega API for J.M.Dev
@@ -38,3 +44,6 @@ app.use('/api/v1/eskart/series', seriesController)
 
 // nerdmeme
 app.use('/api/v1/nerdmeme/meme', memeController)
+
+// trekdex
+app.use('/api/v1/trekdex', trekDexController)
