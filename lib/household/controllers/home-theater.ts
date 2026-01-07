91017b4ab61d33937268aacc670f6aebd6330209
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2026-01-07 09:39:15 -0800

    add controller for sofabaton API

---
+++ lib/household/controllers/home-theater.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+export default Router()
+  .get('/set-sleep-timer', (req, res) => {
+    console.log('TV SLEEP TIMER INITIATED');
+    let response = null
+    const shutdownUrl: string = process.env.STEAMING_TV_SHUTDOWN_URL as string
+    setTimeout(async() => {
+      response = await fetch('https://app1.sofabaton.com/app/keypress?node_id=fpRXbAn3WZxJWDj5qpKQbA&id=fpRXbAn101&type=0')
+      if(response) {
+        console.log('TIMED PROCESS EXECUTED')
+        res.send(response)
+      }
+    }, 90000)
+  })
